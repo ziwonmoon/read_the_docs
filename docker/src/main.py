b10dd42423673ce3a737759c7a234963a3245368
ziwonmoon <ziwonmoon@gmail.com>
2025-05-26 23:49:11 +0900

    MCP서버 띄우기 성공

--- docker/src/main.py
+++ docker/src/main.py
@@ -1,1 +1,10 @@
-print("hello world!")
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("read_the_docs")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    return a + b
+
+if __name__ == "__main__":
+    mcp.run()
